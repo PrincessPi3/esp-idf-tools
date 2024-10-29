eff6783ad2404b7006da2639398186b1bfa6e32d
PrincessPi3 <human.bagel@gmail.com>
2024-10-29 06:33:13 -0600

    edrfgr

---
+++ custom_bin/export.py
@@ -0,0 +1,3 @@
+import os
+os.environ['SILLY'] = 'IAMASILLYFILLY'
+print("SILLY = ", os.environ['SILLY'])
--- test-scratch.sh
+++ test-scratch.sh
@@ -1,10 +1,23 @@
-#!/bin/bash
-set -e
+subprocess() {
+    echo "\nChanging ESPBAUD\n\t1: 9600\n\t2: 115200\n\t3: 230400\n\t4: 460800\n\t5: 1152000\n\t6: 1500000\n\nEnter Selection: "
+    read baudRate
+    echo "\n"
+    case $baudRate in
+    1) selection=9600 ;;
+    2) selection=115200 ;;
+    3) selection=230400 ;;
+    4) selection=460800 ;;
+    5) selection=1152000 ;;
+    6) selection=1500000 ;;
+    esac
 
-echo "Script executed from: ${PWD}"
+    return $selection
+}
 
-BASEDIR=$(dirname $0)
-echo "Script location: ${BASEDIR}"
+subprocess
+ret=$?
 
-DIR="$( cd "$( dirname "$0" )" && pwd )"
-echo $DIR
+export ESPBAUD=$ret
+
+echo "\nBaudrate set to $ESPBAUD\n"
+echo "\nAll done :3\n"
