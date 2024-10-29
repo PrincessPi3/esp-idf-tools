ff77f2938ad018ac82fc27a0c4b1ba81d3dd85bc
PrincessPi3 <human.bagel@gmail.com>
2024-10-29 07:16:23 -0600

    funngleyay

--- custom_bin/changeesp.sh
+++ custom_bin/changeesp.sh
@@ -1,8 +1,15 @@
-echo "\nChanging ESPTARGET\n"
-echo "Set esp target (esp32, esp32s3, esp32c6, esp8266, etc)"
-read esp
+subprocess() {
+    echo "\nChanging ESPTARGET\n"
+    echo "Set esp target (esp32, esp32s3, esp32c6, esp8266, etc)"
+    read esp
+    eval "$1=$esp"
+    return 0
+}
 
-export ESPTARGET="${esp}"
+ret=''
+subprocess ret
+
+export ESPTARGET="${ret}"
 
 echo "\nESPTARGET set to ${ESPTARGET}\n"
 echo "\nall done :3\n"
