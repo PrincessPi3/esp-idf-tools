b5ffb138c40260538e771b8fa95e7e27c1d3f501
PrincessPi3 <human.bagel@gmail.com>
2024-11-12 19:21:26 -0700

    61-alpha3.2

--- alias.sh
+++ alias.sh
@@ -4,11 +4,16 @@ echo 'alias esp_monitor="tail -n 75 -f $ESPIDF_INSTALLDIR/install.log;"' >> ~/.z
 
 echo 'alias esp_logs="less $ESPIDF_INSTALLDIR/install.log; less $ESPIDF_INSTALLDIR/version-data.txt"' >> ~/.zshrc
 
+# run_esp_reinstall performs a git pull first to get the latest esp-inststall-custom code, then cats version.txt for clarity before running the command with an optional argument
 # run_esp_reinstall
 # run_esp_reinstall clear
 # run_esp_reinstall nuke
 # run_esp_reinstall test
 # run_esp_reinstall interactive
 # run_esp_reinstall retool
+
+# esp_monitor is an alias for tail -n 75 -f $ESPIDF_INSTALLDIR/install.log and takes no arguments
 # esp_monitor
-# esp_logs
+
+# esp_logs is an alias for less $ESPIDF_INSTALLDIR/install.log; less $ESPIDF_INSTALLDIR/version-data.txt and takes no arguments
+# esp_logs 
