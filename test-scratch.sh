d1a46a60ad1a5513ca8752a994d0507789a0f929
PrincessPi3 <human.bagel@gmail.com>
2024-11-13 02:21:59 -0700

    61-beta4.3.4

--- test-scratch.sh
+++ test-scratch.sh
@@ -1,5 +1,27 @@
-alias get_idf 2>/dev/null
+alias get_idf #2>/dev/null
 ret=$?
 if [ $ret -eq 1 ]; then
-	echo "aliasnant not found"
+	echo "get_idf not found"
+fi
+
+alias run_esp_reinstall #2>/dev/null
+ret=$?
+if [ $ret -eq 1 ]; then
+	echo "run_esp_reinstall not found"
+fi
+
+alias esp_monitor #2>/dev/null
+ret=$?
+if [ $ret -eq 1 ]; then
+	echo "esp_monitor not found"
+fi
+
+alias esp_logs #2>/dev/null
+ret=$?
+if [ $ret -eq 1 ]; then
+	echo "esp_monitor not found"
+fi
+
+if [ -z $ESPIDF_INSTALLDIR ]; then
+	echo "ESPIDF_INSTALLDIR not found"
 fi
