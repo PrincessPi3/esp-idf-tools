5d0a6745dba4353be267aa53ecf12a1e6f68b0b8
PrincessPi3 <human.bagel@gmail.com>
2024-11-12 18:49:39 -0700

    61-alpha2

---
+++ alias.sh
@@ -0,0 +1,5 @@
+echo 'alias run_esp_reinstall="git -C $HOME/esp/esp-install-custom pull; cat $HOME/esp/esp-install-custom/version.txt; bash $HOME/esp/esp-install-custom/reinstall-esp-idf.sh "' >> ~/.zshrc
+
+echo 'alias esp_monitor="tail -n 75 -f $ESPIDF_INSTALLDIR/install.log;"' >> ~/.zshrc
+
+echo 'alias esp_logs="less $ESPIDF_INSTALLDIR/install.log; less $ESPIDF_INSTALLDIR/version-data.txt"' >> ~/.zshrc
