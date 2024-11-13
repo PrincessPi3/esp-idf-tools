be586ca9589a381ab78b7173df492e63610ca5d7
PrincessPi3 <human.bagel@gmail.com>
2024-11-13 02:12:33 -0700

    61-beta4.3.1

--- test-scratch.sh
+++ test-scratch.sh
@@ -1,23 +1,5 @@
-subprocess() {
-    echo "\nChanging ESPBAUD\n\t1: 9600\n\t2: 115200\n\t3: 230400\n\t4: 460800\n\t5: 1152000\n\t6: 1500000\n\nEnter Selection: "
-    read baudRate
-    echo "\n"
-    case $baudRate in
-    1) selection=9600 ;;
-    2) selection=115200 ;;
-    3) selection=230400 ;;
-    4) selection=460800 ;;
-    5) selection=1152000 ;;
-    6) selection=1500000 ;;
-    esac
-
-    return $selection
-}
-
-subprocess
+alias get_idf
 ret=$?
-
-export ESPBAUD=$ret
-
-echo "\nBaudrate set to $ESPBAUD\n"
-echo "\nAll done :3\n"
+if [ $ret -eq 1 ]; then
+	echo aliasnant not found
+fi
