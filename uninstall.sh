7096cf28207cf4866aa1ce2baeda56c5c8d425da
PrincessPi3 <human.bagel@gmail.com>
2025-08-18 19:43:43 -0600

    bugfixan before i end us all

--- uninstall.sh
+++ uninstall.sh
@@ -2,6 +2,7 @@
 echo STARTING UNINSTALLATION
 rm -rf ~/esp/esp-idf 2>/dev/null
 rm -rf ~/esp/esp-dev-kits 2>/dev/null
+rm -rf ~/esp/esp-idf-tools 2>/dev/null
 rm -rf ~/.espressif 2>/dev/null
 echo DONE
 # handle removing aliases from ~/.bashrc
