1187567c29eb70a9244675806848ed1aa71099b2
PrincessPi3 <human.bagel@gmail.com>
2025-08-18 20:03:02 -0600

    m0ar fizax

--- uninstall.sh
+++ uninstall.sh
@@ -4,5 +4,7 @@ rm -rf ~/esp/esp-idf 2>/dev/null
 rm -rf ~/esp/esp-dev-kits 2>/dev/null
 rm -rf ~/esp/esp-idf-tools 2>/dev/null
 rm -rf ~/.espressif 2>/dev/null
+rm -f ~/esp/install.log 2>/dev/null
+rm -f ~/esp/version-data.log 2>/dev/null
 echo DONE
 # handle removing aliases from ~/.bashrc
